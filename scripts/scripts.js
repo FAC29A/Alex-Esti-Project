5bc407ca30fd9ecf82ba81d5846d2c95aa867fdb
Alexander <alex@alexvoiceover.com>
2023-10-15 19:02:23 +0100

    Added default latitude and longitude

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,38 @@
 let latitude = 51.55848;
 let longitude = -0.11337;
+const myForm = document.querySelector("form");
+const mapElement = document.getElementById("map");
+let map; // Declare the map variable outside of the functions
 
 document.addEventListener("DOMContentLoaded", function () {
   // Wait for the DOM to be ready
-  // Initialize the map when the DOM is ready
-  updateMap(latitude, longitude);
+  initializeMap();
+  myForm.addEventListener("submit", handleFormSubmit);
 });
 
-function updateMap(varLatitude, varLongitude) {
-  //setView([coordinates], ZoomLevel);
-  var map = L.map("map").setView([varLatitude, varLongitude], 33);
+function initializeMap() {
+  // Initialize the map at the beginning
+  map = L.map("map").setView([latitude, longitude], 13);
+
   // Add the OpenStreetMap tile layer
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 }
 
-const myForm = document.querySelector("form");
-myForm.addEventListener("submit", (event) => {
+function handleFormSubmit(event) {
   event.preventDefault();
   const formData = new FormData(myForm);
-  // Convert FormData into a plain object
   const formObject = Object.fromEntries(formData);
 
-  // Extract latitude and longitude values from the form
   const newLatitude = parseFloat(formObject.latitude);
   const newLongitude = parseFloat(formObject.longitude);
 
-  // Call updateMap with the new latitude and longitude
   updateMap(newLatitude, newLongitude);
-});
+}
+
+function updateMap(varLatitude, varLongitude) {
+  map.setView([varLatitude, varLongitude], 13);
+  console.log(`Updating map`);
+}
