109b7e44c55280f01ceb1735da5c91eed820cc42
Alexander <alex@alexvoiceover.com>
2023-10-16 17:36:04 +0100

    Added data.js

---
+++ scripts/data.js
@@ -0,0 +1,19 @@
+//data.js
+
+crimes = [
+  { url: "all-crime", name: "All crime", image: "" },
+  { url: "anti-social-behaviour", name: "Anti-social behaviour" },
+  { url: "bicycle-theft", name: "Bicycle theft" , image:""},
+  { url: "burglary", name: "Burglary" },
+  { url: "criminal-damage-arson", name: "Criminal damage and arson" },
+  { url: "drugs", name: "Drugs" },
+  { url: "other-theft", name: "Other theft" },
+  { url: "possession-of-weapons", name: "Possession of weapons" },
+  { url: "public-order", name: "Public order" },
+  { url: "robbery", name: "Robbery" },
+  { url: "shoplifting", name: "Shoplifting" },
+  { url: "theft-from-the-person", name: "Theft from the person" },
+  { url: "vehicle-crime", name: "Vehicle crime" },
+  { url: "violent-crime", name: "Violence and sexual offences" },
+  { url: "other-crime", name: "Other crime" },
+];
